e6d4af4be3595a0e6a8c7005d9f0741d813f3597
Wolf Dániel <wolf.dani9@gmail.com>
2023-05-22 16:16:29 +0200

    implemented threading on GameController

--- src/GameController.java
+++ src/GameController.java
@@ -1,43 +1,54 @@
-public class GameController {
+public class GameController extends Thread{
     private final int BOARD_SIZE = 3; //with another move solution in Npc this can be changed
     private final Npc wizard;
     private final Npc fighter;
     private String board;
 
     public GameController() {
-        this.wizard = new Npc('V', 6);
-        this.fighter = new Npc('H', 6);
+        this.wizard = new Npc('V', 6,2);
+        this.fighter = new Npc('H', 6,0);
         updateBoard();
+    }
+
+    @Override
+    public void run(){
+        printGame();
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        while(!isFinished()) {
+            makeTurn();
+            printGame();
+            if(isFinished()) {
+                printWinner();
+            }
+            try {
+                synchronized (this) {
+                    wait();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void makeTurn() {
         wizard.move();
         fighter.move();
         updateBoard();
-
         if(isBattle()) {
             battle();
         }
     }
 
-    public void gameLoop() {
-        printGame();
-        while(!isFinished()) {
-            makeTurn();
-            printGame();
-        }
-        printWinner();
-    }
-
-    public void setInitialPositions() {
-        wizard.setPosition(2);
-        fighter.setPosition(0);
-    }
-
     private void updateBoard(){
         StringBuilder board = new StringBuilder();
         for(int i = 0; i < BOARD_SIZE; i++) {
-            if(wizard.getPosition() == i && wizard.getPosition() == fighter.getPosition()) {
+            if(wizard.getPosition() == i && wizard.compareTo(fighter) == 0) {
                 board.append('X');
             } else if (wizard.getPosition() == i) {
                 board.append(wizard.print());
@@ -50,19 +61,27 @@ public class GameController {
         this.board = board.toString();
     }
 
+    public String getBoard() {
+        return board;
+    }
+
     public String getGameStatus() {
         String status = board +
                 " --> ";
         if(isBattle()) {
             status += "harc: ";
         }
-        status += fighter.printHealth() +
-                ", " +
-                wizard.printHealth();
+        status += getHitPoints();
         return status;
     }
 
-    private boolean isBattle(){
+    public String getHitPoints(){
+        return fighter.printHealth() +
+                "," +
+                wizard.printHealth();
+    }
+
+    public boolean isBattle(){
         for (char field: board.toCharArray()) {
             if(field == 'X') {
                 return true;
@@ -76,11 +95,11 @@ public class GameController {
         fighter.takeDamage(wizard.giveDamage());
     }
 
-    private boolean isFinished() {
+    public boolean isFinished() {
         return wizard.getHitPoints() <= 0 || fighter.getHitPoints() <= 0;
     }
 
-    private String declareWinner() {
+    public String getWinner() {
         if(wizard.getHitPoints() > 0) {
             return "A Varázsló nyert!";
         } else if(fighter.getHitPoints() > 0) {
@@ -91,7 +110,7 @@ public class GameController {
     }
 
     private void printWinner() {
-        System.out.println(declareWinner());
+        System.out.println(getWinner());
     }
 
     public void printGame() {
