30e35072b7422ac5898fceb1a28b671796e1e9f1
Wolf Dániel <wolf.dani9@gmail.com>
2023-05-22 16:13:50 +0200

    implemented comparable on Npc

--- src/Npc.java
+++ src/Npc.java
@@ -1,13 +1,14 @@
-public class Npc {
+public class Npc implements Comparable<Npc> {
   private final char playingPiece;
   private int hitPoints;
   private final Dice dice;
   private int position;
 
-  public Npc(char playingPiece, int diceSide) {
+  public Npc(char playingPiece, int diceSide, int position) {
     this.playingPiece = playingPiece;
     this.dice = new Dice(diceSide);
     this.hitPoints = dice.throwDice() + 3;
+    this.position = position;
   }
 
   public void takeDamage(int damage) {
@@ -26,10 +27,6 @@ public class Npc {
     return hitPoints;
   }
 
-  public void setPosition(int position) {
-    this.position = position;
-  }
-
   public int getPosition() {
     return position;
   }
@@ -40,13 +37,16 @@ public class Npc {
       case 1, 2 -> this.position = 0;
       case 3, 4 -> this.position = 1;
       case 5, 6 -> this.position = 2;
-      default -> {
-      //To-Do: throw new UnexpectedMoveException
-      }
+      default -> {}
     }
   }
 
   public String print() {
     return String.valueOf(playingPiece);
+  }
+
+  @Override
+  public int compareTo(Npc npc) {
+    return this.position - npc.position;
   }
 }
